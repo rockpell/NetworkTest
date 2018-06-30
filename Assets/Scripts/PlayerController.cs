a1270575589ae4fc9712987b2a4deefb457f45ea
rockpell <aythffk@gmail.com>
2018-07-01 01:15:17 +0900

    network transform rigidbody로 대상 변경 이동 방식 rigidbody로 변경

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,35 +8,49 @@ public class PlayerController : NetworkBehaviour {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
 
-	// Use this for initialization
-	void Start () {
+    private Vector3 realPosition = Vector3.zero;
+    private Quaternion realRotation;
+    private Vector3 sendPosition = Vector3.zero;
+    private Quaternion sendRotation;
 
+    private Rigidbody myRigidbody;
+
+    // Use this for initialization
+    void Start () {
+        myRigidbody = GetComponent<Rigidbody>();
     }
 	
 	// Update is called once per frame
 	void Update () {
         if (!isLocalPlayer)
         {
+            //transform.position = Vector3.Lerp(transform.position, realPosition, Time.deltaTime * 1.5f);
+            //transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, Time.deltaTime * 3f);
             return;
         }
         //float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
-        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f * 1;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f * 2;
 
         //transform.Rotate(0, x, 0);
         //transform.Translate(0, 0, z);
-        transform.Translate(x, 0, z);
+        //transform.Translate(x, 0, z);
+
+        myRigidbody.MovePosition(transform.position + transform.forward * z + transform.right * x);
+        //myRigidbody.AddForce(transform.forward * z + transform.right * x * 10f);
+        //Debug.Log("dfdfd:   " + (transform.forward * z + transform.right * x * 10f));
         MouseControl();
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            CmdFire();
+            CmdFire(bulletSpawn.position, bulletSpawn.rotation);
         }
     }
 
     [Command]
-    private void CmdFire()
+    private void CmdFire(Vector3 spawnPosition, Quaternion spawnRotation)
     {
-        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        //GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        GameObject bullet = Instantiate(bulletPrefab, spawnPosition, spawnRotation);
         bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 30;
         NetworkServer.Spawn(bullet);
         Destroy(bullet, 2.0f);
@@ -55,7 +69,8 @@ public class PlayerController : NetworkBehaviour {
 
         //vertical = Mathf.Clamp(vertical, -80, 80);
 
-        transform.Rotate(0, horizontal, 0);
-        
+        //transform.Rotate(0, horizontal, 0);
+        Quaternion deltaRotation = Quaternion.Euler(new Vector3(0, horizontal, 0));
+        myRigidbody.MoveRotation(myRigidbody.rotation * deltaRotation);
     }
 }
